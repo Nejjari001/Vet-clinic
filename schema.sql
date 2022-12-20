f7fe56d6886753ae8f3af42a09b67b3e40a47ddb
Nejjari Abdelhafid <mr.nejjari.abdelhafid@gmail.com>
2022-12-20 12:01:19 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE DATABASE vet_clinic;
-
 CREATE TABLE animals (
     id BIGSERIAL NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL
+    name VARCHAR(50),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );
