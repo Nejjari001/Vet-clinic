ce19d275c771fb2d82fdf50699226c1d47997ade
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-12-21 20:29:24 +0100

    Insert data in data.sql file

--- data.sql
+++ data.sql
@@ -5,5 +5,14 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', date '01-17-2021', '15.04', 'false', '1');
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', date '05-12-2017', '11', 'true', '5');
 
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', date '02-08-2020', '11', 'false', '0');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Plantmon', date '11-15-2021', '5.7', 'true', '2');
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Squirtle', date '05-02-1993', '12.13', 'false', '3');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Angemon', date '06-12-2005', '45', 'true', '1');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Boarmon', date '06-07-2005', '20.4', 'true', '7');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Blossom', date '10-13-1998', '17', 'true', '3');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Ditto', date '05-14-2022', '22', 'true', '4');
+
 
 
