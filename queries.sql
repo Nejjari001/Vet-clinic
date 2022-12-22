cf4db35a681188e132c7cd5bab38ae9c0a8f1e9f
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-12-22 12:16:27 +0100

    Add Query questions to queris.sql file

--- queries.sql
+++ queries.sql
@@ -26,3 +26,11 @@ UPDATE animals SET weight_kg = (weight_kg * -1);
 ROLLBACK TO savepoint_1;
 UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
 COMMIT;
+
+/*Query questions*/
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT name FROM animals ORDER BY escape_attempts DESC LIMIT 1;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '01/01/1990' AND '01/01/2000'  GROUP BY species;
