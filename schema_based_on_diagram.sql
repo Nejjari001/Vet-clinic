1c6d349eac1f6c41bae379adfa99716143484f1a
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-11 19:51:24 +0500

    Add treatments table with the Id property type and name and also create table for treatment history which have treatment_id and histories_id used as a foreign key

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,16 @@ patient_id INT
 REFERENCES patient(id) ON DELETE CASCADE,
 status VARCHAR(250) NOT NULL
 );
+
+
+CREATE TABLE treatments (
+	id SERIAL NOT NULL PRIMARY KEY,
+	type VARCHAR(250) NOT NULL,
+	name VARCHAR(250) NOT NULL
+);
+
+
+CREATE TABLE treatment_histories(
+   treatment_id INT REFERENCES treatments(id) ON DELETE CASCADE,
+   histories_id INT REFERENCES medical_histories(id) ON DELETE CASCADE
+);
