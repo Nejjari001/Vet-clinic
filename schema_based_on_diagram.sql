e400be478bdeaeb137d65054bf0ea02efe380788
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-01-11 19:06:49 +0100

    Update schema_based_on_diagrame

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,24 +1,22 @@
 CREATE TABLE patients(
-    id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(250) NOT NULL,
-    date_of_birth DATE
+	id SERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(250) NOT NULL,
+	date_of_birth DATE
 );
 
 CREATE TABLE medical_histories(
-id SERIAL NOT NULL PRIMARY KEY,
-admitted_at TIMESTAMP,
-patient_id INT
-REFERENCES patient(id) ON DELETE CASCADE,
-status VARCHAR(250) NOT NULL
+	id SERIAL NOT NULL PRIMARY KEY,
+	admitted_at TIMESTAMP,
+	patient_id INT 
+	REFERENCES patients(id) ON DELETE CASCADE,
+	status VARCHAR(250) NOT NULL
 );
-
 
 CREATE TABLE treatments (
 	id SERIAL NOT NULL PRIMARY KEY,
 	type VARCHAR(250) NOT NULL,
 	name VARCHAR(250) NOT NULL
 );
-
 
 CREATE TABLE treatment_histories(
    treatment_id INT REFERENCES treatments(id) ON DELETE CASCADE,
