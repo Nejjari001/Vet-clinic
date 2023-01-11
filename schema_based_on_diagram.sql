915a1d9819465e063645411a60685790c914b462
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-01-11 15:48:03 +0100

    Add invoices and invoices_items TABLES

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -24,3 +24,30 @@ CREATE TABLE treatment_histories(
    treatment_id INT REFERENCES treatments(id) ON DELETE CASCADE,
    histories_id INT REFERENCES medical_histories(id) ON DELETE CASCADE
 );
+
+CREATE TABLE invoices (
+  id BIGSERIAL PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT,
+  CONSTRAINT fk_medical_histories
+  FOREIGN KEY (medical_history_id)
+  REFERENCES medical_histories(id)
+);
+
+-- Create invoices_items table
+CREATE TABLE invoices_items (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  CONSTRAINT fk_invoices
+  FOREIGN KEY (invoice_id)
+  REFERENCES invoices(id),
+  CONSTRAINT fk_treatments
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments(id)
+); 
